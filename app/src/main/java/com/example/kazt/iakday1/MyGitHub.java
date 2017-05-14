fc25ef788da5f8daeaf267e4f9cfb0a0cbf0310b
Nur Hidayat Agung <agung7659@students.amikom.ac.id>
2017-05-14 12:09:00 +0700

    update

--- app/src/main/java/com/example/kazt/iakday1/MyGitHub.java
+++ app/src/main/java/com/example/kazt/iakday1/MyGitHub.java
@@ -1,14 +1,20 @@
 package com.example.kazt.iakday1;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.kazt.iakday1.databinding.ActivityMyGitHubBinding;
+import com.example.kazt.iakday1.netService.GitHubService;
+
 public class MyGitHub extends AppCompatActivity {
+    private GitHubService service;
+    private ActivityMyGitHubBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_git_hub);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_my_git_hub);
     }
 }
